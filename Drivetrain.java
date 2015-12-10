a949dde0f9a8e38038ed550084451513c1bf4422
Guest <guest-k6nh2T@cslab4-21.stuy.edu>
2015-12-10 17:35:14 -0500

    brian

---
+++ Drivetrain.java
@@ -0,0 +1,9 @@
+public class Drivetrain
+	private Bumbo frontLeftMotor 
+	private Bumbo rearLeftMotor
+	private Bumbo frontRightMotor
+	private Bumbo rearRightMotor
+	private int speed;
+	private int weight;
+
+	public static void main(String[] args){
